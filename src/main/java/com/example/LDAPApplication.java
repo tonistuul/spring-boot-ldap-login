ab6945c3ee0a8587ff0cf97480e78f560bdb8048
tonistuul <tonis.tuul@gmail.com>
2015-10-28 22:07:22 +0200

    Set up Spring Boot

---
+++ src/main/java/com/example/LDAPApplication.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by tonis on 26-Oct-15.
+ */
+@SpringBootApplication
+public class LDAPApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LDAPApplication.class, args);
+    }
+}
